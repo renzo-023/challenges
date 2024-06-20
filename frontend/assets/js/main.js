e074d4bb17fe5ba0ecd6eaa7b709ef3dbda193b4
renzo-023 <renzolicom@gmail.com>
2024-06-20 00:36:54 -0300

    create menu

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -15,3 +15,11 @@ if(navClose){
     });
 };
 
+/* mudando com scroll */
+function scrollHeader() {
+    const header = document.getElementById("header");
+    if(this.scrollY >=50) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
